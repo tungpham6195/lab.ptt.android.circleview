aa65c4bc9415674ea3d673d862b22464f71bcbe2
tungpham6195 <phamthanhtung1995@gmail.com>
2017-10-30 23:46:41 +0700

    init

---
+++ app/src/main/java/com/app/ptt/circleview/CircleView.java
@@ -0,0 +1,31 @@
+package com.app.ptt.circleview;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by PTT on 10/30/2017.
+ */
+
+public class CircleView extends View {
+    public CircleView(Context context) {
+        super(context);
+    }
+
+    public CircleView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CircleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+    }
+}
