e53e036688e2437d55774454f54e63ea35d1ce91
tungpham6195 <phamthanhtung1995@gmail.com>
2017-11-03 00:42:03 +0700

    nothing

--- app/src/main/java/com/app/ptt/circleview/CircleView.java
+++ app/src/main/java/com/app/ptt/circleview/CircleView.java
@@ -2,6 +2,8 @@ package com.app.ptt.circleview;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -11,6 +13,10 @@ import android.view.View;
  */
 
 public class CircleView extends View {
+    Paint red_paint_fill,
+            blue_paint_fill,
+            green_paint_fill;
+
     public CircleView(Context context) {
         super(context);
     }
@@ -21,11 +27,27 @@ public class CircleView extends View {
 
     public CircleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        red_paint_fill = new Paint();
+        red_paint_fill.setStyle(Paint.Style.FILL);
+        red_paint_fill.setColor(Color.RED);
+
+        blue_paint_fill = new Paint();
+        blue_paint_fill.setStyle(Paint.Style.FILL);
+        blue_paint_fill.setColor(Color.BLUE);
+
+        green_paint_fill = new Paint();
+        green_paint_fill.setStyle(Paint.Style.FILL);
+        green_paint_fill.setColor(Color.GRAY);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
+//        Arc
+//        canvas.dr
     }
 }
