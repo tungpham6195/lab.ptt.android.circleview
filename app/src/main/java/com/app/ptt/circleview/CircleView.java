5cac40ed259ebad89f790a570d541bf243aa74c9
Tung Pham <phamthanhtung1995@gmail.com>
2018-05-30 21:47:41 +0700

    nothing

--- app/src/main/java/com/app/ptt/circleview/CircleView.java
+++ app/src/main/java/com/app/ptt/circleview/CircleView.java
@@ -2,6 +2,9 @@ package com.app.ptt.circleview;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Rect;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -11,6 +14,8 @@ import android.view.View;
  */
 
 public class CircleView extends View {
+    Paint red_paint_fill;
+
     public CircleView(Context context) {
         super(context);
     }
@@ -21,11 +26,19 @@ public class CircleView extends View {
 
     public CircleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        red_paint_fill = new Paint();
+        red_paint_fill.setStyle(Paint.Style.FILL);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
+        Rect cirle_1=new Rect();
+        cirle_1.set(100,100,150,160);
+        canvas.drawRect);
     }
 }
--- app/src/main/java/com/app/ptt/circleview/MainActivity.java
+++ app/src/main/java/com/app/ptt/circleview/MainActivity.java
@@ -4,10 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    CircleView circleView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        circleView = (CircleView) findViewById(R.id.circleview);
+
     }
 }
